d048b2eb2c3ab97318b192551ded0269150706c2
Minzoo Kim <wkxh18@gmail.com>
2018-02-28 03:21:17 +0900

    진짜민주간다

---
+++ theatre/static/js/search.js
@@ -0,0 +1,59 @@
+// BOXOFFICE 1st
+$(document).ready(function(){
+    $('.carousel').carousel({padding:250, dist:0});
+
+    $(".card_wrap_1").hover(function(){
+        $(".card_info_1").addClass("card_info_hover");
+        $(".card_image_1").addClass("card_image_hover");
+    });
+
+    $(".card_wrap_1").mouseleave(function(){
+        $('.card_info_1').removeClass("card_info_hover");
+        $(".card_image_1").removeClass("card_image_hover");
+    });
+
+    // BOXOFFICE 2nd
+    $(".card_wrap_2").hover(function(){
+        $(".card_info_2").addClass("card_info_hover");
+        $(".card_image_2").addClass("card_image_hover");
+    });
+
+    $(".card_wrap_2").mouseleave(function(){
+        $('.card_info_2').removeClass("card_info_hover");
+        $(".card_image_2").removeClass("card_image_hover");
+    });
+
+    // BOXOFFICE 3rd
+    $(".card_wrap_3").hover(function(){
+        $(".card_info_3").addClass("card_info_hover");
+        $(".card_image_3").addClass("card_image_hover");
+    });
+
+    $(".card_wrap_3").mouseleave(function(){
+        $('.card_info_3').removeClass("card_info_hover");
+        $(".card_image_3").removeClass("card_image_hover");
+    });
+
+    // BOXOFFICE 4th
+    $(".card_wrap_4").hover(function(){
+        $(".card_info_4").addClass("card_info_hover");
+        $(".card_image_4").addClass("card_image_hover");
+    });
+
+    $(".card_wrap_4").mouseleave(function(){
+        $('.card_info_4').removeClass("card_info_hover");
+        $(".card_image_4").removeClass("card_image_hover");
+    });
+
+    // BOXOFFICE 5th
+    $(".card_wrap_5").hover(function(){
+        $(".card_info_5").addClass("card_info_hover");
+        $(".card_image_5").addClass("card_image_hover");
+    });
+
+    $(".card_wrap_5").mouseleave(function(){
+        $('.card_info_5').removeClass("card_info_hover");
+        $(".card_image_5").removeClass("card_image_hover");
+    });
+});
+
